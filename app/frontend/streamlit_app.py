c0fef8e048caae97c32b83dad6816dfdfe7f56aa
Danish Ali <danishrao2528@gmail.com>
2026-08-07 16:26:57 +0500

    Fix Streamlit imports

--- app/frontend/streamlit_app.py
+++ app/frontend/streamlit_app.py
@@ -1,12 +1,8 @@
 import streamlit as st
 
-from app.frontend.auth_page import (
-    show_auth_page,
-)
-from app.frontend.dashboard import (
-    show_dashboard,
-)
-from app.frontend.resume_pages import (
+from auth_page import show_auth_page
+from dashboard import show_dashboard
+from resume_pages import (
     ai_analysis_page,
     create_resume_page,
     delete_resume_page,
@@ -14,9 +10,7 @@ from app.frontend.resume_pages import (
     show_all_resumes,
     update_resume_page,
 )
-from app.frontend.sidebar import (
-    show_sidebar,
-)
+from sidebar import show_sidebar
 
 
 st.set_page_config(
@@ -36,38 +30,28 @@ if "update_resume" not in st.session_state:
 
 
 if st.session_state.token is None:
-
     show_auth_page()
 
 else:
-
     page = show_sidebar()
 
-
     if page == "Dashboard":
-
         show_dashboard()
 
     elif page == "All Resumes":
-
         show_all_resumes()
 
     elif page == "Add Resume":
-
         create_resume_page()
 
     elif page == "Find Resume":
-
         search_resume_page()
 
     elif page == "Update Resume":
-
         update_resume_page()
 
     elif page == "Delete Resume":
-
         delete_resume_page()
 
     elif page == "AI Analysis":
-
         ai_analysis_page()
